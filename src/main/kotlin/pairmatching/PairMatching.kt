665c475ebbd12710666a7c760811e737f9717897
Yerin <myheaven_b@naver.com>
2022-12-16 18:21:22 +0900

    feat: add 입력에 따른 정해진 서비스 수행하는 기능

--- src/main/kotlin/pairmatching/PairMatching.kt
+++ src/main/kotlin/pairmatching/PairMatching.kt
@@ -2,8 +2,17 @@ package pairmatching
 
 class PairMatching {
     private val data = Data()
+    private val matchingService = MatchingService()
 
     fun start() {
         data.readFile()
+        while (true) {
+            when (matchingService.choose()) {
+                Service.ONE.getChoice() -> matchingService.getMatching()
+                Service.TWO.getChoice() -> matchingService.showPairs()
+                Service.THREE.getChoice() -> matchingService.initPairs()
+                Service.QUIT.getChoice() -> break
+            }
+        }
     }
 }
